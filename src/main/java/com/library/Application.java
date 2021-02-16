3c386d4626d1700f42077fa291120aeadb742abc
Lifestyler <antone3479@gmail.com>
2021-02-16 23:49:21 +0300

    Remove H2 database, use PostgreSQL

--- src/main/java/com/library/Application.java
+++ src/main/java/com/library/Application.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource({"classpath*:applicationContext.xml"})
-public class PetProjectLibraryApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(PetProjectLibraryApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
